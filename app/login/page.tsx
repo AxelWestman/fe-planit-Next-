a48c1d7c7a2643612b32f5bb820249ecc42f27d2
AxelW <axelwestman@outlook.com>
2025-11-25 23:10:36 -0300

    Listo el register

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,19 @@
+
 import React from 'react'
 import FormLogin from '@/components/FormLogin'
 import Carousel from '@/components/Carousel'
+import FormRegister from '@/components/FormRegister'
+import LoginRegister from '@/components/LoginRegister'
 
 type Props = {}
 
 function page({}: Props) {
+
+
   return (
      <>
           <main className="w-full flex min-h-screen bg-[#F7F9F8] bg-center">
-                  <FormLogin />
+                  <LoginRegister />
                   <Carousel />
           </main>
     </>
